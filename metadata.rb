95317044511c9025c0dec8ab556dfa901a65bd4d
Jan Klare <j.klare@cloudbau.de>
2017-02-23 17:07:12 +0100

    starting ocata development patch
    Change-Id: Ibbbfc1008f9e7688466431ab1168740c97f162ee

--- metadata.rb
+++ metadata.rb
@@ -7,12 +7,12 @@ source_url 'https://github.com/openstack/cookbook-openstack-orchestration' if re
 license 'Apache 2.0'
 description 'Installs and configures the Heat Service'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '14.0.0'
+version '15.0.0'
 
 %w(ubuntu redhat centos).each do |os|
   supports os
 end
 
-depends 'openstack-common', '>= 14.0.0'
-depends 'openstack-identity', '>= 14.0.0'
+depends 'openstack-common', '>= 15.0.0'
+depends 'openstack-identity', '>= 15.0.0'
 depends 'openstackclient'
