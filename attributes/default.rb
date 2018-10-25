b7b6d7fede7cd020e3a224aee4b79c21744af46b
Roger Luethi <rl@patchworkscience.org>
2018-10-25 09:26:37 +0200

    Add openstack-orchestration::dashboard recipe
    Starting with Queens, upstream has moved the Orchestration (heat)
    dashboard from the main dashboard package into a separate package.
    
    Change-Id: Ifb2018623bd4df250c2d3d8c5a7473f6cff94fe8
    (cherry picked from commit dd41637548dbb4bbdb114c81d7666bc692ef363f)

--- attributes/default.rb
+++ attributes/default.rb
@@ -63,6 +63,7 @@ when 'rhel'
   default['openstack']['orchestration']['platform'] = {
     'heat_common_packages' => ['openstack-heat-common'],
     'heat_api_packages' => ['openstack-heat-api'],
+    'heat_dashboard_packages' => ['openstack-heat-ui'],
     'heat_api_service' => 'openstack-heat-api',
     'heat_api_cfn_packages' => ['openstack-heat-api-cfn'],
     'heat_api_cfn_service' => 'openstack-heat-api-cfn',
@@ -77,6 +78,7 @@ when 'debian'
   default['openstack']['orchestration']['platform'] = {
     'heat_common_packages' => ['heat-common'],
     'heat_api_packages' => ['heat-api'],
+    'heat_dashboard_packages' => ['python-heat-dashboard'],
     'heat_api_service' => 'heat-api',
     'heat_api_cfn_packages' => ['heat-api-cfn'],
     'heat_api_cfn_service' => 'heat-api-cfn',
