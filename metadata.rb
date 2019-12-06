0b218495a92284af3b91a2f9123e6ed8fdfd9d33
Lance Albertson <lance@osuosl.org>
2019-12-05 20:52:31 -0800

    Updates for rocky
    - Replace git.openstack.org with opendev.org
    - Update some documentation
    - Move README.md to README.rst for better rendering
    - Drop obsolete bootstrap.sh script
    
    Change-Id: If47b6b8c525c92a9421468e371c22f210b7f5e80

--- metadata.rb
+++ metadata.rb
@@ -3,17 +3,22 @@ maintainer       'openstack-chef'
 maintainer_email 'openstack-discuss@lists.openstack.org'
 license          'Apache-2.0'
 description      'Installs and configures the Heat Service'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '18.0.0'
+
+recipe 'openstack-orchestration::api-cfn', 'Configure and start heat-api-cfn service'
+recipe 'openstack-orchestration::api', 'Configure and start heat-api service'
+recipe 'openstack-orchestration::common', 'Installs the heat packages and setup configuration for Heat.'
+recipe 'openstack-orchestration::engine', 'Setup the heat database and start heat-engine service'
+recipe 'openstack-orchestration::identity_registration', 'Registers the Heat API endpoint, heat service and user'
 
 %w(ubuntu redhat centos).each do |os|
   supports os
 end
 
+depends 'openstackclient'
 depends 'openstack-common', '>= 18.0.0'
 depends 'openstack-identity', '>= 18.0.0'
-depends 'openstackclient'
 
-issues_url 'https://launchpad.net/openstack-chef' if respond_to?(:issues_url)
-source_url 'https://github.com/openstack/cookbook-openstack-orchestration' if respond_to?(:source_url)
-chef_version '>= 12.5' if respond_to?(:chef_version)
+issues_url 'https://launchpad.net/openstack-chef'
+source_url 'https://opendev.org/openstack/cookbook-openstack-orchestration'
+chef_version '>= 14.0'
